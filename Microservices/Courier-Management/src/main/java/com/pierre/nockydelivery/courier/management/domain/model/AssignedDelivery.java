85bd3280945f2150c92e130952af56773ae2aeff
pierre enoc <pierre_enoc@yahoo.fr>
2025-07-23 20:43:45 -0300

    add domain courier

---
+++ Microservices/Courier-Management/src/main/java/com/pierre/nockydelivery/courier/management/domain/model/AssignedDelivery.java
@@ -0,0 +1,31 @@
+package com.pierre.nockydelivery.courier.management.domain.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@EqualsAndHashCode
+public class AssignedDelivery {
+
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private OffsetDateTime assignedAt;
+
+    @Getter(AccessLevel.PRIVATE)
+    private Courier courier;
+
+    static AssignedDelivery pending(UUID deliveryId, Courier courier) {
+        AssignedDelivery delivery = new AssignedDelivery();
+        delivery.setId(deliveryId);
+        delivery.setAssignedAt(OffsetDateTime.now());
+        //delivery.setCourier(courier);
+        return delivery;
+    }
+}
