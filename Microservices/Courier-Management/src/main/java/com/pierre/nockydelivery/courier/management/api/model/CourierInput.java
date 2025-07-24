b0fb63058a4a9fdd396045063fb2ed4356ac9301
pierre enoc <pierre_enoc@yahoo.fr>
2025-07-24 10:32:24 -0300

    add crud

---
+++ Microservices/Courier-Management/src/main/java/com/pierre/nockydelivery/courier/management/api/model/CourierInput.java
@@ -0,0 +1,15 @@
+package com.pierre.nockydelivery.courier.management.api.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CourierInput {
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String phone;
+}
+
