df6b4677be2a0aa241d92bbb6e6785df0392b15f
pierre enoc <pierre_enoc@yahoo.fr>
2025-07-24 08:55:24 -0300

    add post, put

--- Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/api/controller/DeliveryController.java
+++ Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/api/controller/DeliveryController.java
@@ -2,25 +2,33 @@ package com.pierre.nockydelivery.delivery.traking.api.controller;
 
 import com.pierre.nockydelivery.delivery.traking.domain.model.Delivery;
 import com.pierre.nockydelivery.delivery.traking.domain.model.DeliveryInput;
+import com.pierre.nockydelivery.delivery.traking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/v1/deliveries")
+@RequiredArgsConstructor
 public class DeliveryController {
+
+    private final DeliveryPreparationService deliveryPreparationService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Delivery draft(@RequestBody @Valid DeliveryInput input) {
 
-        return null;
+        return deliveryPreparationService.draft(input);
     }
 
     @PutMapping("/{deliveryId}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Delivery edit(@RequestBody @Valid DeliveryInput input) {
+    @ResponseStatus(HttpStatus.OK)
+    public Delivery edit(@PathVariable UUID deliveryId,
+                         @RequestBody @Valid DeliveryInput input) {
 
-        return null;
+        return deliveryPreparationService.edit(deliveryId, input);
     }
 }
--- Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/domain/service/DeliveryPreparationService.java
+++ Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/domain/service/DeliveryPreparationService.java
@@ -1,4 +1,79 @@
 package com.pierre.nockydelivery.delivery.traking.domain.service;
 
+import com.pierre.nockydelivery.delivery.traking.domain.exception.DomainException;
+import com.pierre.nockydelivery.delivery.traking.domain.model.*;
+import com.pierre.nockydelivery.delivery.traking.domain.repository.DeliveryRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class DeliveryPreparationService {
+
+    private final DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public Delivery draft(DeliveryInput input) {
+        Delivery delivery = Delivery.draft();
+        handlePreparation(input, delivery);
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    @Transactional 
+    public Delivery edit(UUID deliveryId, DeliveryInput input) {
+        Delivery delivery = deliveryRepository.findById(deliveryId)
+                .orElseThrow(() -> new DomainException("Delivery not found with id: " + deliveryId));
+        delivery.removeItems();
+        handlePreparation(input, delivery);
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    private void handlePreparation(DeliveryInput input, Delivery delivery) {
+        ContactPointInput senderInput = input.getSender();
+        ContactPointInput recipientInpunt = input.getRecipient();
+
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode(senderInput.getZipCode())
+                .street(senderInput.getStreet())
+                .number(senderInput.getNumber())
+                .complement(senderInput.getComplement())
+                .name(senderInput.getName())
+                .phone(senderInput.getPhone())
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode(recipientInpunt.getZipCode())
+                .street(recipientInpunt.getStreet())
+                .number(recipientInpunt.getNumber())
+                .complement(recipientInpunt.getComplement())
+                .name(recipientInpunt.getName())
+                .phone(recipientInpunt.getPhone())
+                .build();
+
+        Duration expectedDeliveryTime = Duration.ofHours(3);
+        BigDecimal distanceFee = new BigDecimal("10.00");
+
+        BigDecimal payout = new BigDecimal("10.00");
+
+
+        var preparationsDetails = Delivery.PreparationDetails.builder()
+                .recipient(recipient)
+                .sender(sender)
+                .expectedDeliveryTime(expectedDeliveryTime)
+                .courierPayout(payout)
+                .distanceFee(distanceFee)
+                .build();
+
+        delivery.editPreparationDetails(preparationsDetails);
+
+        for (ItemInput itemInput : input.getItems()) {
+           delivery.addItem(itemInput.getName(), itemInput.getQuantity());
+        }
+    }
+
 }
