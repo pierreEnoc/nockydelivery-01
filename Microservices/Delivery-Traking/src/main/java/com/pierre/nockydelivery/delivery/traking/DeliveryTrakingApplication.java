8b24a7f4e47a40991168c8b0e7f731461c12c522
pierre enoc <pierre_enoc@yahoo.fr>
2025-07-25 14:06:28 -0300

    add eureuka config

--- Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/DeliveryTrakingApplication.java
+++ Microservices/Delivery-Traking/src/main/java/com/pierre/nockydelivery/delivery/traking/DeliveryTrakingApplication.java
@@ -2,8 +2,10 @@ package com.pierre.nockydelivery.delivery.traking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DeliveryTrakingApplication {
 
 	public static void main(String[] args) {
